9826fd90f2c1f8757efe48d53694dea6f0860fc8
MatthewMcConnell <matthewmcconnell04@gmail.com>
2024-01-17 22:08:06 +0100

    Started to get into rebalancing the tree

--- src/avl/node.cpp
+++ src/avl/node.cpp
@@ -1,7 +1,7 @@
 #include "node.hpp"
 
 namespace avl {
-    node::node(const int value) : value(value), left(nullptr), right(nullptr) {}
+    node::node(const int value) : value(value), left(nullptr), right(nullptr), height(1) {}
 
     bool node::insert(const int value) {
         if (this->value == value) {
@@ -22,10 +22,40 @@ namespace avl {
     bool node::insertIntoSubtree(std::unique_ptr<node>& subtree, const int value) {
         if (subtree == nullptr) {
             subtree = std::make_unique<node>(value);
+            height++;
+            rebalance();
             return true;
         }
 
         return subtree->insert(value);
+    }
+
+    void node::rebalance() {
+        // todo we need to check if we have a problem when we insert something in the middle of the tree, or if that's
+        // even possible
+        int heightDifference = left->height - right->height;
+        if (heightDifference > 1) {
+            rotateRight();
+        } else if (heightDifference < -1) {
+            rotateLeft();
+        }
+    }
+
+    void node::rotateRight() {
+        // todo
+        height--;
+        left->height++;
+
+        if (left->right != nullptr) {
+            // wait, how do we handle the pointer to this node from the parent?
+
+            right = std::move(left->right);
+            left->right = nullptr;
+        }
+    }
+
+    void node::rotateLeft() {
+        // todo
     }
 
     bool node::subtreeContains(const std::unique_ptr<node>& subtree, const int value) {
--- src/avl/node.hpp
+++ src/avl/node.hpp
@@ -9,9 +9,14 @@ namespace avl {
         [[maybe_unused]] int value;
         std::unique_ptr<node> left;
         std::unique_ptr<node> right;
+        int height;
 
-        static bool insertIntoSubtree(std::unique_ptr<node>& subtree, const int value);
         static bool subtreeContains(const std::unique_ptr<node>& subtree, const int value);
+        
+        bool insertIntoSubtree(std::unique_ptr<node>& subtree, const int value);
+        void rebalance();
+        void rotateRight();
+        void rotateLeft(); 
 
       public:
         node(const int value);
