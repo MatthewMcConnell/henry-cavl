d9e9e6da0e7b8c8bc9d28e9dd2ad438ee344ade8
MatthewMcConnell <matthewmcconnell04@gmail.com>
2024-02-03 17:52:39 +0100

    Insertion completely finished for AVL tree

--- src/avl/node.hpp
+++ src/avl/node.hpp
@@ -10,33 +10,80 @@ namespace avl {
         node* right = nullptr;
         int height = 1;
 
-        void rebalance();
-        void rotateRight();
-        void rotateLeft();
+        static node* rotateRight(node* oldRoot) {
+            node* newRoot = oldRoot->left;
+            oldRoot->left = newRoot->right;
+            newRoot->right = oldRoot;
 
+            updateHeight(oldRoot);
+            updateHeight(newRoot);
+
+            return newRoot;
+        }
+
+        static node* rotateLeft(node* oldRoot) {
+            node* newRoot = oldRoot->right;
+            oldRoot->right = newRoot->left;
+            newRoot->left = oldRoot;
+
+            updateHeight(oldRoot);
+            updateHeight(newRoot);
+
+            return newRoot;
+        }
+
+        /**
+         * @return std::optional<node*> - the node created or that was already present in the tree, otherwise empty
+         */
         static std::optional<node*> doNormalBSTInsertion(node* root, int value) {
             if (root == nullptr) {
-                return std::optional<node*>(new node(value));
+                return std::optional(new node(value));
             }
 
-            if (value == root->getValue()) {
-                return std::optional<node*>(root);
+            if (value == root->value) {
+                return std::optional(root);
             }
 
-            if (value < root->getValue()) {
+            if (value < root->value) {
                 root->left = insert(root->left, value);
             }
 
-            if (value > root->getValue()) {
+            if (value > root->value) {
                 root->right = insert(root->right, value);
             }
 
             return std::nullopt;
         }
 
+        static void updateHeight(node* root) {
+            root->height = 1 + std::max(getHeight(root->left), getHeight(root->right));
+        }
+
+        static int getHeight(node* root) { return root == nullptr ? 0 : root->height; }
+
+        static node* rebalance(node* root, int value) {
+            int balance = getBalance(root);
+
+            if (balance > 1) {
+                if (value > root->left->value) {
+                    root->left = rotateLeft(root->left);
+                }
+                return rotateRight(root);
+            } else if (balance < -1) {
+                if (value < root->right->value) {
+                    root->right = rotateRight(root->right);
+                }
+                return rotateLeft(root);
+            }
+
+            return root;
+        }
+
+        static int getBalance(node* root) { return getHeight(root->left) - getHeight(root->right); }
+
       public:
         node(int value) : value(value) {}
-        int getValue() const { return this->value; }
+        int getValue() const { return value; }
 
         static node* insert(node* root, int value) {
             std::optional<node*> newNode = doNormalBSTInsertion(root, value);
@@ -45,7 +92,9 @@ namespace avl {
                 return newNode.value();
             }
 
-            return root;
+            updateHeight(root);
+
+            return rebalance(root, value);
         }
 
         static bool contains(node* root, int value) {
